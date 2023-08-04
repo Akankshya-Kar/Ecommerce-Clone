4450f698be9685bcc23d5ae158ffbbaea0ff6c0a
Akankshya Kar <akankshyakar5391@gmail.com>
2023-08-04 21:20:47 +0530

    Initial draft to main branch

---
+++ Models/Home/HomeIndexViewModel.cs
@@ -0,0 +1,38 @@
+﻿using E_Commerce.Data;
+using E_Commerce.Repository;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.SessionState;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.Mvc;
+using PagedList;
+using PagedList.Mvc;
+
+
+
+namespace E_Commerce.Models.Home
+{
+    public class HomeIndexViewModel
+    {
+        public GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
+
+        ECommerceEntities context = new ECommerceEntities();
+
+        public IPagedList<Tbl_Product> ListOfProducts { get; set; }
+
+        public HomeIndexViewModel CreateModel(string search, int pageSize, int? page)
+        {
+            SqlParameter[] para = new SqlParameter[] {
+                new SqlParameter("@search",search??(object)DBNull.Value)
+            };
+            IPagedList<Tbl_Product> data = context.Database.SqlQuery<Tbl_Product>("GetBySearch @search", para).ToList().ToPagedList(page ?? 1, pageSize);
+            return new HomeIndexViewModel
+            {
+                ListOfProducts = data
+            };
+    }
+    }
+}
