47e3c07496a7b623a5365545ae5ec16f8f7e2c32
Akankshya Kar <akankshyakar5391@gmail.com>
2023-08-06 15:12:25 +0530

    Added Cart functionalities

---
+++ Models/Home/Item.cs
@@ -0,0 +1,14 @@
+﻿using E_Commerce.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace E_Commerce.Models.Home
+{
+    public class Item
+    {
+        public Tbl_Product Product {  get; set; }
+        public int Quantity {  get; set; }
+    }
+}
